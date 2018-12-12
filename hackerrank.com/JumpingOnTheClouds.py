a3f2561c955a8abc65379615f23f8c27c534047a
Christopher Carlson <ccarlson91@gmail.com>
2018-12-12 13:46:35 -0800

    Finished Problem.

--- hackerrank.com/JumpingOnTheClouds.py
+++ hackerrank.com/JumpingOnTheClouds.py
@@ -6,7 +6,32 @@ import re
 import sys
 
 # Complete the jumpingOnClouds function below.
+
+    
 def jumpingOnClouds(c):
+     
+
+    jumps = 0
+    end = len(c) -1
+    currentCloud = 0
+    
+ 
+        
+    while currentCloud < end:
+        
+        if currentCloud + 2 < end + 1  and c[currentCloud + 2] == 0:
+            
+            currentCloud += 2
+            jumps += 1
+        
+        elif currentCloud + 1 < end + 1  and c[currentCloud + 1] == 0:
+            
+            currentCloud +=  1
+            jumps += 1
+        
+        
+        
+    return jumps
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
