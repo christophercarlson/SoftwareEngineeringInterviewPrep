f9bce8f9c83e97882b9c1a981e30452194d0afd0
Christopher Carlson <ccarlson91@gmail.com>
2019-05-06 11:52:06 -0700

    Update quickSort.java

--- AlgorithmsAndDataStructures/algorithmsAndDataStructures/src/algorithms/quickSort.java
+++ AlgorithmsAndDataStructures/algorithmsAndDataStructures/src/algorithms/quickSort.java
@@ -2,4 +2,25 @@ package algorithms;
 
 public class quickSort {
 
+
+
+
+
+    static int i = 1;
+   
+    public static void main(String args[])
+     {
+          System.out.print(i + ",");
+          m(i);
+          System.out.println(i);
+     }
+    
+    
+    
+    
+    public static void m(int i)
+     {
+        i += 2;                              
+     }
+    
 }
