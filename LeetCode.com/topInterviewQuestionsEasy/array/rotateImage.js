5df4d3a51318bc7e6aa71df595f0c6b1c1c6d60d
Christopher Carlson <ccarlson91@gmail.com>
2020-02-12 13:50:31 -0800

    still working on it done for today.

--- LeetCode.com/topInterviewQuestionsEasy/array/rotateImage.js
+++ LeetCode.com/topInterviewQuestionsEasy/array/rotateImage.js
@@ -46,9 +46,36 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 
+let matrix = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8]
+  ];
 
 
- 
 var rotate = function(matrix) {
+   
+    let cur;
+
+    // Nested loop.
+    for(let i = 0; i < matrix.length; i++ ){
+        
+        for(let j = i; j< matrix[0].length; j++ ){
+            
+            // Have to make current index here before current index gets altered. 
+            cur = matrix[i][j];
+            
+            matrix[i][j]= matrix[j][i];
+            matrix[j][i]= cur;
+        }
+    }
+    for(let i=0;i<matrix.length;i++){
+        
+        matrix[i].reverse();
+    }
     
 };
+
+rotate(matrix);
+console.table(matrix);
+
