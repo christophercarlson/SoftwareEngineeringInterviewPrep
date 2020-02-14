e5790e5201dcb8f266b9d1f96f4c35ba728620eb
Christopher Carlson <ccarlson91@gmail.com>
2020-02-14 08:59:50 -0800

    Update rotateImage.js

--- LeetCode.com/topInterviewQuestionsEasy/array/rotateImage.js
+++ LeetCode.com/topInterviewQuestionsEasy/array/rotateImage.js
@@ -69,14 +69,12 @@ var rotate = function(matrix) {
             
             matrix[i][j]= matrix[j][i];
             matrix[j][i]= currentIndex;
-            console.table(matrix)
         }
     }
     for(let i=0;i<matrix.length;i++){
         
         matrix[i].reverse();
-    }
-    
+    } 
 };
 
 rotate(matrix);
