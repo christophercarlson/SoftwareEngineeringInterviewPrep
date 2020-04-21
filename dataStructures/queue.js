dd12cfe929ffaa54d91c47b5ed2ff0ec8ff782be
Christopher Carlson <ccarlson91@gmail.com>
2020-04-20 17:34:57 -0700

    chipped a little bit more away.

--- dataStructures/queue.js
+++ dataStructures/queue.js
@@ -18,3 +18,105 @@
 
 // 9. SEARCH You might need to search a queue to check whether an element exists within a queue. Again, this involves creating a buffer queue first to avoid modifications to the original queue.
 
+
+// Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+// One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+// Your implementation should support following operations:
+
+// MyCircularQueue(k): Constructor, set the size of the queue to be k.
+// Front: Get the front item from the queue. If the queue is empty, return -1.
+// Rear: Get the last item from the queue. If the queue is empty, return -1.
+// enQueue(value): Insert an element into the circular queue. Return true if the operation is successful.
+// deQueue(): Delete an element from the circular queue. Return true if the operation is successful.
+// isEmpty(): Checks whether the circular queue is empty or not.
+// isFull(): Checks whether the circular queue is full or not.
+
+
+"use strict";
+
+/*
+ * Initialize your data structure here. Set the size of the queue to be k.
+ * @param {number} k
+ */
+class MyCircularQueue  {
+
+    constructor(k){
+    //initialize new que and set the head and tail to begginning.
+    this.que = new Array(k);
+    this.head = 0;
+    this.tail = 0;
+    }
+    
+};
+
+/*
+ * Insert an element into the circular queue. Return true if the operation is successful. 
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function(value) {
+    if( this.length > 0){
+
+    }
+};
+
+/*
+ * Delete an element from the circular queue. Return true if the operation is successful.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function() {
+    
+};
+
+/*
+ * Get the front item from the queue.
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function() {
+    
+};
+
+/*
+ * Get the last item from the queue.
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function() {
+    
+};
+
+/*
+ * Checks whether the circular queue is empty or not.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function() {
+    
+};
+
+/*
+ * Checks whether the circular queue is full or not.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function() {
+    
+};
+
+/** 
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
+
+
+
+//  TEST AREA:
+
+let k = 10;
+
+let que = new MyCircularQueue(k);
