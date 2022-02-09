119cdf08ce5975c04d913ea47b535d56b6047fda
Christopher Carlson <ccarlson91@gmail.com>
2022-02-09 09:28:54 -0800

    Update twoNumberSum.js

--- algoExpert/twoNumberSum.js
+++ algoExpert/twoNumberSum.js
@@ -1,6 +1,23 @@
+
+let log = console.log
+let array = [3, 5, -4, 8, 11, 1, -1, 6]
+let targetSum = 10
+
+
+
 function twoNumberSum(array, targetSum) {
-  // Write your code here.
+
+  for (let x = 0; x < array.length; x++) {
+    for (let y = array.length - 1; y > x; y--) {
+      if (array[x] + array[y] == targetSum) {
+        return [array[x], array[y]]
+      }
+    }
+  }
+  return []
 }
 
-// Do not edit the line below.
-exports.twoNumberSum = twoNumberSum;
+
+
+console.log(twoNumberSum(array, targetSum))
+
